a9f1050d7b8979fb0ad0db5034ae191b9dd92db6
hogimn <hogimn@gmail.com>
2026-07-05 18:39:34 +0900

    Add anime and poll flow test

--- applications/integration/src/test/kotlin/test/hogimn/malchart/FlowTest.kt
+++ applications/integration/src/test/kotlin/test/hogimn/malchart/FlowTest.kt
@@ -14,16 +14,27 @@ class FlowTest {
     val template = RestTemplate()
 
     lateinit var discovery: Process
-    lateinit var allocations: Process
-    lateinit var backlog: Process
-    lateinit var registration: Process
-    lateinit var timesheets: Process
+    lateinit var anime: Process
+    lateinit var poll: Process
 
     @Before
     fun setUp() {
         val userDir = System.getProperty("user.dir")
 
         RedisConfig().getClient("localhost", "foobared").flushAll()
+
+        JdbcTemplate(
+            DataSourceConfig()
+                .createDataSource("jdbc:mysql://localhost:3306/anime_test?user=uservices&password=uservices")
+        ).apply {
+            execute("delete from anime")
+        }
+        JdbcTemplate(
+            DataSourceConfig()
+                .createDataSource("jdbc:mysql://localhost:3306/poll_test?user=uservices&password=uservices")
+        ).apply {
+            execute("delete from poll")
+        }
 
         discovery = runCommand(
             8880,
@@ -32,39 +43,30 @@ class FlowTest {
             File(userDir)
         )
 
-        JdbcTemplate(DataSourceConfig().createDataSource("jdbc:mysql://localhost:3306/allocations_test?user=uservices&password=uservices")).apply {
-            execute("delete from allocations")
-        }
-        JdbcTemplate(DataSourceConfig().createDataSource("jdbc:mysql://localhost:3306/backlog_test?user=uservices&password=uservices")).apply {
-            execute("delete from stories")
-        }
-        JdbcTemplate(DataSourceConfig().createDataSource("jdbc:mysql://localhost:3306/registration_test?user=uservices&password=uservices")).apply {
-            execute("delete from projects")
-            execute("delete from accounts")
-            execute("delete from users")
-        }
-        JdbcTemplate(DataSourceConfig().createDataSource("jdbc:mysql://localhost:3306/timesheets_test?user=uservices&password=uservices")).apply {
-            execute("delete from time_entries")
-        }
-
-        allocations = runCommand(8881, "allocations", "java -jar $userDir/../allocations-server/build/libs/allocations-server.jar", File(userDir))
-        backlog = runCommand(8882, "backlog", "java -jar $userDir/../backlog-server/build/libs/backlog-server.jar", File(userDir))
-        registration = runCommand(8883, "registration","java -jar $userDir/../registration-server/build/libs/registration-server.jar", File(userDir))
-        timesheets = runCommand(8884, "timesheets", "java -jar $userDir/../timesheets-server/build/libs/timesheets-server.jar", File(userDir))
+        anime = runCommand(
+            8881,
+            "anime",
+            "java -jar $userDir/../anime-server/build/libs/anime-server.jar",
+            File(userDir)
+        )
+        poll = runCommand(
+            8882,
+            "poll",
+            "java -jar $userDir/../poll-server/build/libs/poll-server.jar",
+            File(userDir)
+        )
     }
 
     @After
     fun tearDown() {
         discovery.destroy()
-        allocations.destroy()
-        backlog.destroy()
-        registration.destroy()
-        timesheets.destroy()
+        anime.destroy()
+        poll.destroy()
     }
 
     @Test
     fun testBasicFlow() {
-        listOf(8880, 8881, 8882, 8883, 8884).forEach { waitUntilReady("http://localhost:$it") }
+        listOf(8880, 8881, 8882).forEach { waitUntilReady("http://localhost:$it") }
 
         var response: String?
 
@@ -72,92 +74,80 @@ class FlowTest {
         response = template.get(discoveryServer, "application/json")
         assertEquals("Noop!", response)
 
+        val animeServer = "http://localhost:8881"
 
-
-        val registrationServer = "http://localhost:8883"
-
-        response = template.get(registrationServer, "application/json")
+        response = template.get(animeServer, "application/json")
         assertEquals("Noop!", response)
 
-        response = template.post("$registrationServer/registration", "application/json", """{"name": "aUser"}""")
-        val aUserId = findResponseId(response)
-        assert(aUserId.toLong() > 0)
+        val animeId = "9999"
+        val newAnimeJson = """
+            {
+              "id": $animeId,
+              "title": "New E2E Test Anime",
+              "link": "https://example.com/anime/$animeId",
+              "image": "https://example.com/img/$animeId.jpg",
+              "score": 8.8,
+              "members": 120000,
+              "genre": "Sci-Fi, Action",
+              "studios": "Trigger",
+              "source": "Original",
+              "season": "SUMMER",
+              "year": 2026,
+              "rank": 100,
+              "popularity": 250,
+              "scoringCount": 95000,
+              "episodes": 12,
+              "airStatus": "Currently Airing",
+              "type": "TV",
+              "startDate": "2026-07-01T00:00:00",
+              "endDate": "2026-09-23T00:00:00",
+              "englishTitle": "New Anime English",
+              "japaneseTitle": "New Anime Japanese",
+              "synopsis": "This is an E2E testing anime.",
+              "largeImage": "https://example.com/img/${animeId}_large.jpg",
+              "rating": "PG-13",
+              "nsfw": "SFW"
+            }
+        """.trimIndent()
 
-        response = template.get("$registrationServer/users", "application/json", Pair("userId", aUserId))
-        assert(response.isNotEmpty())
+        response = template.post("$animeServer/anime", "application/json", newAnimeJson)
+        assert(response.contains("anime created"))
 
-        response = template.get("$registrationServer/accounts", "application/json", Pair("ownerId", aUserId))
-        val anAccountId = findResponseId(response)
-        assert(anAccountId.toLong() > 0)
+        response = template.get("$animeServer/anime", "application/json", Pair("id", animeId))
+        assert(response.contains("New E2E Test Anime"))
 
-        response = template.post(
-            "$registrationServer/projects",
-            "application/vnd.appcontinuum.v2+json",
-            """{"accountId":"$anAccountId","name":"aProject","active":true,"funded":true}"""
-        )
-        val aProjectId = findResponseId(response)
-        assert(aProjectId.toLong() > 0)
+        val pollServer = "http://localhost:8882"
+
+        response = template.get(pollServer, "application/json")
+        assertEquals("Noop!", response)
+
+        val topicId = "707"
+        val pollOptionId = "1"
+        val newPollJson = """
+            {
+              "contentId": $animeId,
+              "contentType": "anime",
+              "topicId": $topicId,
+              "pollOptionId": $pollOptionId,
+              "title": "Which character is the best?",
+              "episode": 1,
+              "votes": 1250
+            }
+        """.trimIndent()
+
+        response = template.post("$pollServer/poll", "application/json", newPollJson)
+        assert(response.contains("poll created"))
 
         response = template.get(
-            "$registrationServer/projects",
-            "application/vnd.appcontinuum.v2+json",
-            Pair("accountId", anAccountId)
-        )
-        assert(response.isNotEmpty())
-
-
-
-        val allocationsServer = "http://localhost:8881"
-
-        response = template.get(allocationsServer, "application/json")
-        assertEquals("Noop!", response)
-
-        response = template.post(
-            "$allocationsServer/allocations",
+            "$pollServer/poll",
             "application/json",
-            """{"projectId":$aProjectId,"userId":$aUserId,"firstDay":"2015-05-17","lastDay":"2015-05-26"}"""
+            Pair("contentId", animeId),
+            Pair("contentType", "anime"),
+            Pair("topicId", topicId),
+            Pair("pollOptionId", pollOptionId)
         )
-        val anAllocationId = findResponseId(response)
-        assert(anAllocationId.toLong() > 0)
-
-        response = template.get("$allocationsServer/allocations", "application/json", Pair("projectId", aProjectId))
-        assert(response.isNotEmpty())
-
-
-
-        val backlogServer = "http://localhost:8882"
-
-        response = template.get(backlogServer, "application/json")
-        assertEquals("Noop!", response)
-
-        response = template.post(
-            "$backlogServer/stories",
-            "application/json",
-            """{"projectId":$aProjectId,"name":"A story"}"""
-        )
-        val aStoryId = findResponseId(response)
-        assert(aStoryId.toLong() > 0)
-
-        response = template.get("$backlogServer/stories", "application/json", Pair("projectId", aProjectId))
-        assert(response.isNotEmpty())
-
-
-
-        val timesheetsServer = "http://localhost:8884"
-
-        response = template.get(timesheetsServer, "application/json")
-        assertEquals("Noop!", response)
-
-        response = template.post(
-            "$timesheetsServer/time-entries",
-            "application/json",
-            """{"projectId":$aProjectId,"userId":$aUserId,"date":"2015-05-17","hours":"8"}"""
-        )
-        val aTimeEntryId = findResponseId(response)
-        assert(aTimeEntryId.toLong() > 0)
-
-        response = template.get("$timesheetsServer/time-entries", "application/json", Pair("userId", aUserId))
-        assert(response.isNotEmpty())
+        assert(response.contains("Which character is the best?"))
+        assert(response.contains("poll info"))
     }
 
     /// Test Support
@@ -172,10 +162,6 @@ class FlowTest {
             }
         }
         throw IllegalStateException("Server at $url was not ready within ${timeoutMillis}ms")
-    }
-
-    private fun findResponseId(response: String): String {
-        return Regex("id\":(\\d+),").find(response)?.groupValues!![1]
     }
 
     private fun runCommand(port: Int, services: String, command: String, workingDir: File): Process {
